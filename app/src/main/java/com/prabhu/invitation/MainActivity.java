259c6a8e8256d7238536fadd96f874c10f7222a0
Prabhu <prabhuk56@gmail.com>
2017-12-10 16:59:29 +0530

    Added crashlytics and Firebase

--- app/src/main/java/com/prabhu/invitation/MainActivity.java
+++ app/src/main/java/com/prabhu/invitation/MainActivity.java
@@ -2,15 +2,15 @@ package com.prabhu.invitation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Fabric.with(this, new Crashlytics());
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/great_vibes.ttf")
                 .setFontAttrId(R.attr.fontPath)
